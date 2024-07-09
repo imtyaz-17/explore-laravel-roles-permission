2160e84d9b33861cc1cc8ea7192bccdfb9c8a6bd
imtyaz-17 <imtyazit17017@gmail.com>
2024-07-09 20:32:37 +0600

    Permission - model relations created

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Permission extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'user_roles');
+    }
+    public function hasRole($role)
+    {
+        return $this->roles->contains('name', $role);
+    }
 }
