5b876a9494154a7a80c504ecd8cda4b0f1e92087
imtyaz-17 <imtyazit17017@gmail.com>
2024-07-09 19:56:47 +0600

    user-role db table created

--- database/migrations/2024_07_09_134850_create_user_roles_table.php
+++ database/migrations/2024_07_09_134850_create_user_roles_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('user_roles', function (Blueprint $table) {
+        Schema::create('user_roles', function (Blueprint $table) {
             //
             $table->id();
             $table->foreignId('user_id')->constrained();
@@ -25,8 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('user_roles', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('user_roles');
     }
 };
